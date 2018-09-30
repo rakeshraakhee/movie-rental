b1698ac6d3c85556cd0d73403379e542a245a9c1
x228394 <x228394@SY4-6T0HKC2.LUV.AD.SWACORP.com>
2018-09-30 10:57:51 -0500

    Move amountOf method to rental class

--- movie-rental-hw2-git/src/Customer.java
+++ movie-rental-hw2-git/src/Customer.java
@@ -30,7 +30,7 @@ public class Customer {
 					Rental each       = (Rental) rentals.nextElement();
 
 					// determine amounts for each line
-					thisAmount = amountOf(each);
+					thisAmount = each.amountOf();
 					
 					// add frequent renter points
 					frequentRenterPoints++;
@@ -54,26 +54,5 @@ public class Customer {
 				return result;
 	}
 
-	private double amountOf(Rental each) {
-		double thisAmount = 0;
-		switch (each.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			thisAmount += 2;
-			if (each.getDaysRented() > 2) {
-				thisAmount += (each.getDaysRented() - 2) * 1.5;
-			}
-			break;
-		case Movie.NEW_RELEASE:
-			thisAmount += each.getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			thisAmount += 1.5;
-			if (each.getDaysRented() > 3) {
-				thisAmount += (each.getDaysRented() - 3) * 1.5;
-			}
-			break;
-		}
-
-		return thisAmount;
-	}
+	
 }
--- movie-rental-hw2-git/src/Rental.java
+++ movie-rental-hw2-git/src/Rental.java
@@ -14,4 +14,27 @@ public class Rental {
     public Movie getMovie() {
         return _movie;
     }
+    
+    public double amountOf() {//*variable name Change---each to aRental
+		double aRentalAmount = 0;//*variable name Change---thisAmount to aRentalAmount
+		switch (getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			aRentalAmount += 2;
+			if (getDaysRented() > 2) {
+				aRentalAmount += (getDaysRented() - 2) * 1.5;
+			}
+			break;
+		case Movie.NEW_RELEASE:
+			aRentalAmount += getDaysRented() * 3;
+			break;
+		case Movie.CHILDRENS:
+			aRentalAmount += 1.5;
+			if (getDaysRented() > 3) {
+				aRentalAmount += (getDaysRented() - 3) * 1.5;
+			}
+			break;
+		}
+
+		return aRentalAmount;
+	}
 }
